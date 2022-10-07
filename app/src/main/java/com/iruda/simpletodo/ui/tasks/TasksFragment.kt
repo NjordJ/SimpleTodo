1708dd67b1e53566e4b402e0242f4d6782d26869
NjordJ <nj0rd3841@gmail.com>
2022-10-07 20:17:24 +0500

    Room search in tasks list

--- app/src/main/java/com/iruda/simpletodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/iruda/simpletodo/ui/tasks/TasksFragment.kt
@@ -1,18 +1,21 @@
 package com.iruda.simpletodo.ui.tasks
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.iruda.simpletodo.R
 import com.iruda.simpletodo.databinding.FragmentTasksBinding
+import com.iruda.simpletodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TasksFragment : Fragment() {
+class TasksFragment : Fragment(), MenuProvider {
     private var _binding: FragmentTasksBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,6 +31,8 @@ class TasksFragment : Fragment() {
     ): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return view
     }
 
@@ -48,6 +53,44 @@ class TasksFragment : Fragment() {
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             tasksAdapter.submitList(it)
+        }
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        // Add menu items here
+        menuInflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.search_action_appbar)
+        val searchView = searchItem.actionView as SearchView
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        // Handle the menu selection
+        return when (menuItem.itemId) {
+            R.id.search_action_appbar -> {
+
+                true
+            }
+            R.id.sort_by_name_action_appbar -> {
+
+                true
+            }
+            R.id.sort_by_date_action_appbar -> {
+
+                true
+            }
+            R.id.hide_completed_tasks_action_appbar -> {
+                menuItem.isChecked = !menuItem.isChecked
+                true
+            }
+            R.id.delete_all_completed_tasks_action_appbar -> {
+
+                true
+            }
+            else -> false
         }
     }
 
--- app/src/main/java/com/iruda/simpletodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/iruda/simpletodo/ui/tasks/TasksViewModel.kt
@@ -4,12 +4,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.iruda.simpletodo.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+
+    val searchQuery = MutableStateFlow("")
+
+    @ExperimentalCoroutinesApi
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
     //Get list of tasks
-    val tasks = taskDao.getTasks().asLiveData()
+    @ExperimentalCoroutinesApi
+    val tasks = tasksFlow.asLiveData()
 }
